949efc2089d435b9d9cd2eceb07e55343bf42aa6
kevinvella <kevin3vel@hotmail.com>
2023-06-26 18:12:17 +0200

    Update Question 2.R

--- Question 2.R
+++ Question 2.R
@@ -1,23 +1,78 @@
-data <- data.frame(
-  Cement = c(540.0, 540.0, 332.5, 332.5, 198.6, 266.0, 380.0, 380.0, 266.0),
-  BlastFurnaceSlag = c(0.0, 0.0, 142.5, 142.5, 132.4, 114.0, 95.0, 95.0, 114.0),
-  Water = c(162.0, 162.0, 228.0, 228.0, 192.0, 228.0, 228.0, 228.0, 228.0),
-  Superplasticizer = c(2.5, 2.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0),
-  CoarseAggregate = c(1040.0, 1055.0, 932.0, 932.0, 978.4, 932.0, 932.0, 932.0, 932.0),
-  FineAggregate = c(676.0, 676.0, 594.0, 594.0, 825.5, 670.0, 594.0, 594.0, 670.0),
-  Age = c(28, 28, 270, 365, 360, 90, 365, 28, 28),
-  ConcreteCompressiveStrength = c(79.99, 61.89, 40.27, 41.05, 44.30, 47.03, 43.70, 36.45, 45.85)
-)
+##### A ######
+data = read.csv('Clustering.csv',header=TRUE) #Clustering.csve
 
-# Step 2: Calculate correlation coefficients
-correlation <- cor(data)
+# Step 1: Scale the dataset
+scaled_data <- scale(data)
 
-# Identify response variable
-response_variable <- names(correlation)[which.max(abs(correlation[,"ConcreteCompressiveStrength"]))]
+# Step 2: Calculate Euclidean distances
+dist_matrix <- dist(scaled_data, method = "euclidean")
 
-# Identify explanatory variables
-explanatory_variables <- names(correlation)[abs(correlation[, "ConcreteCompressiveStrength"]) > 0.2 & names(correlation) != response_variable]
+# Step 3: Create a distance matrix
+dist_matrix <- as.matrix(dist_matrix)
 
-# Print the response variable and explanatory variables
-print(response_variable)
-print(explanatory_variables)
+# Step 4: Plot the heatmap
+install.packages('reshape2')
+library(reshape2)
+library(ggplot2)
+
+heatmap <- ggplot(data = melt(dist_matrix), aes(Var2, Var1, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient(low = "white", high = "darkblue") +
+  theme_minimal() +
+  labs(x = "Data Points", y = "Data Points", title = "Euclidean Distance Heatmap")
+
+print(heatmap)
+
+##### B ######
+# Method 1: Elbow method
+wss <- c()
+for (i in 1:10) {
+  kmeans_result <- kmeans(scaled_data, centers = i, nstart = 10)
+  wss[i] <- kmeans_result$tot.withinss
+}
+
+plot(1:10, wss, type = "b", xlab = "Number of Clusters (k)", ylab = "Within-cluster Sum of Squares")
+
+# Method 2: Silhouette coefficient
+install.packages('tidyclust')
+library(tidyclust)
+silhouette <- c()
+for (i in 2:10) {
+  kmeans_result <- kmeans(scaled_data, centers = i, nstart = 10)
+  silhouette[i] <- silhouette_avg(kmeans_result$cluster, dist(scaled_data))
+}
+
+plot(2:10, silhouette[2:10], type = "b", xlab = "Number of Clusters (k)", ylab = "Average Silhouette Coefficient")
+
+# Method 3: Gap statistic
+library(cluster)
+
+set.seed(123)
+gap_stat <- clusGap(scaled_data, FUNcluster = kmeans, nstart = 10, K.max = 10, B = 50)
+
+plot(gap_stat, main = "Gap Statistic", xlab = "Number of Clusters (k)")
+
+##### C ######
+# Perform k-means clustering
+optimal_k = 2
+kmeans_result <- kmeans(scaled_data, centers = optimal_k, nstart = 10)
+
+# Plot the resulting cluster solution
+library(ggplot2)
+
+cluster_plot <- ggplot(data = data, aes(x = x, y = y, color = as.factor(kmeans_result$cluster))) +
+  geom_point() +
+  scale_color_discrete(name = "Cluster") +
+  theme_minimal() +
+  labs(x = "x", y = "y", title = "Cluster Analysis")
+
+print(cluster_plot)
+
+##### D ######
+# Perform agglomerative clustering
+agglomerative_result <- hclust(dist(scaled_data), method = "complete")
+
+# Plot the dendrogram
+plot(agglomerative_result, main = "Dendrogram", xlab = "Data Points")
+
+
